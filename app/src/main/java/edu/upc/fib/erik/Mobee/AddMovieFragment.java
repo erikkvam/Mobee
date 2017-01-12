e73220657074006b3b61776451f5714141f89e0e
jbpextra <jbpextra@hotmail.com>
2017-01-12 05:40:12 +0100

    some cleaning

--- app/src/main/java/edu/upc/fib/erik/Mobee/AddMovieFragment.java
+++ app/src/main/java/edu/upc/fib/erik/Mobee/AddMovieFragment.java
@@ -25,14 +25,7 @@ import android.widget.TextView;
  * create an instance of this fragment.
  */
 public class AddMovieFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private FilmData filmData;
     private Button saveButton;
     private SeekBar rateBar;
@@ -43,39 +36,20 @@ public class AddMovieFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddMovieFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddMovieFragment newInstance(String param1, String param2) {
-        AddMovieFragment fragment = new AddMovieFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static AddMovieFragment newInstance() {
+        return new AddMovieFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View mView = (View) inflater.inflate(R.layout.fragment_add_movie, container, false);
+        View mView = inflater.inflate(R.layout.fragment_add_movie, container, false);
         MainView act = (MainView) getActivity();
         FloatingActionButton fab = (FloatingActionButton) act.findViewById(R.id.fab);
         fab.setVisibility(View.GONE);
--- app/src/main/java/edu/upc/fib/erik/Mobee/RecyclerViewFragment.java
+++ app/src/main/java/edu/upc/fib/erik/Mobee/RecyclerViewFragment.java
@@ -18,14 +18,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class RecyclerViewFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private RecyclerView recyclerView;
     private static RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -36,51 +29,30 @@ public class RecyclerViewFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecyclerViewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecyclerViewFragment newInstance(String param1, String param2) {
-        RecyclerViewFragment fragment = new RecyclerViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    public static RecyclerViewFragment newInstance() {
+        return new RecyclerViewFragment();
     }
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true);
 
-        // use a linear layout manager
         layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.addItemDecoration(new RecyclerViewDivider(getContext()));
-        // specify an adapter (see also next example)
+
         adapter = new FilmAdapter(((MainView) getActivity()).getFilmData());
 
         recyclerView.setAdapter(adapter);
